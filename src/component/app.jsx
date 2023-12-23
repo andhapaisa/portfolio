564a3812a5296bdf17880a87f3eaf6d14c1de50d
amanak7 <22je0094@iitism.ac.in>
2023-12-24 00:48:49 +0530

    Intial

---
+++ src/component/app.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+   
+const now = new Date().toLocaleTimeString();
+  const [time,inc] = React.useState(now);
+
+  function gettime(){
+    const nown = new Date().toLocaleTimeString();
+    inc(nown);
+  }
+
+setInterval(gettime,1000);
+
+  return (
+    <div className="container">
+      <h1>{time} </h1>
+      <button onClick={gettime}>+!|\/| |_-~</button>
+    </div>
+  );
+}
+
+export default App;
